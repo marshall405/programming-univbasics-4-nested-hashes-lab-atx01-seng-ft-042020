d3644949fa5259b3a74d0818bb8b4cf670a9baee
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-05 22:13:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["Lisp", "C"]
+  }
+  programmer_hash
 end
 
 def changing_alan
@@ -103,7 +107,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +131,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
